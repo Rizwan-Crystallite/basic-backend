4a9b8252b996f25e7ed26d083284cd9242b2d639
dev-mr <m.rizwan@crystal-lite.net>
2024-01-19 04:24:38 +0500

    Fix Route

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
     res.json({
         "login": "hiteshchoudhary",
         "id": 11613311,
